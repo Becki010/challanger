6f3258d270c17ccf4bbafa104930cab31c1ddd08
Lucas Nehrebecki <lucasnehrebecki@gmail.com>
2025-02-06 10:13:01 -0300

    testando finalização

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 let nomes = [];
 
 function adicionarNomes() {
-    let input = document.querySelector("#nomeInput");
+    let input = document.querySelector("nomeAmigo");
     let nome = input.value.trim();
 
 if (nome === "") {
@@ -14,6 +14,24 @@ if (nome === "") {
     input.value = "";
 }
 function atualizarLista() {
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+    
+    for (let amigo of amigos) {
+        let li = document.createElement("li");
+        li.textContent = amigo;
+        lista.appendChild(li);
+    }
+}
 
+function sortearAmigo() {
+    if (amigos.lenght === 0) {
+        alert("A lista está vazia. Adicione um nome antes de sortear");
+        return;
+    }
+    
+    let indiceSorteado = Math.floor(Math.random() * amigos.length);
+    let amigoSorteado = amigos[indiceSorteado];
+    document.getElementById("resultado").innerHTML = `Amigo sorteado: <strong>${amigoSorteado}</strong>`;
 }
     
